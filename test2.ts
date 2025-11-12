a25ad4cdf0c72d471ee28717f380020641386f0c
gkdms5190-bit <gkdms5190@gmail.com>
2025-11-12 11:00:06 +0900

    아무노래나 들어~

--- test2.ts
+++ test2.ts
@@ -1,5 +1,5 @@
-/**배가 고파용 ~~~~~~~~~~~ */
+// /**배가 고파용 ~~~~~~~~~~~ */
 
-let a: number = 1;
-let b: string = `2`;
-console.log(`a 는 :${a}, b 는 ${b}`);
+// let a: number = 1;
+// let b: string = `2`;
+// console.log(`a 는 :${a}, b 는 ${b}`);
