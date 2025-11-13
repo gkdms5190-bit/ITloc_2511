97b531dd9f38ea683dfdcd5181984c3364ef40e6
gkdms5190-bit <gkdms5190@gmail.com>
2025-11-13 10:23:26 +0900

    .

--- test4.ts
+++ test4.ts
@@ -15,3 +15,14 @@ console.log(`str1:${str1}`);
 
 console.log(`array1:`, array1);
 console.log(`jobject:`, jobject1);
+
+console.log(`jobect1의 이름은 :${jobject1.name}, 나이는:${jobject1.age}`);
+
+let mix = [
+  { name: "홍길동", age: 20 }, // 0
+  { name: "이순신", age: 30 }, // 1
+];
+
+console.log(` mix 의 0번째 서랍 : `, mix[0]);
+console.log(` mix 의 1번째 서랍 : `, mix[1]);
+console.log(` mix 의 2번째 서랍 : `, mix[2]);
