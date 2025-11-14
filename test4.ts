72910bb90729aa6faacdf725197bc5b00feda33f
gkdms5190-bit <gkdms5190@gmail.com>
2025-11-14 11:18:28 +0900

    .

--- test4.ts
+++ test4.ts
@@ -1,45 +1,60 @@
-let a = false;
-let b = 0;
+// let a = false;
+// let b = 0;
 
-let c = true;
-let d = 9999;
+// let c = true;
+// let d = 9999;
 
-let str1 = "sss";
+// let str1 = "sss";
 
-let array1 = [3, 1, 9];
+// let array1 = [3, 1, 9];
 
-let jobject1 = { name: "홍길동", age: 20 };
+// let jobject1 = { name: "홍길동", age: 20 };
 
-console.log(`a:${a}`);
-console.log(`str1:${str1}`);
+// console.log(`a:${a}`);
+// console.log(`str1:${str1}`);
 
-console.log(`array1:`, array1);
-console.log(`jobject:`, jobject1);
+// console.log(`array1:`, array1);
+// console.log(`jobject:`, jobject1);
 
-console.log(`jobect1의 이름은 :${jobject1.name}, 나이는:${jobject1.age}`);
+// console.log(`jobect1의 이름은 :${jobject1.name}, 나이는:${jobject1.age}`);
 
-let mix = [
-  { name: "홍길동", age: 20 }, // 0
-  { name: "이순신", age: 30 }, // 1
-];
+// let mix = [
+//   { name: "홍길동", age: 20 }, // 0
+//   { name: "이순신", age: 30 }, // 1
+// ];
 
-console.log(` mix 의 0번째 서랍 : `, mix[0]);
-console.log(` mix 의 1번째 서랍 : `, mix[1]);
-console.log(` mix 의 2번째 서랍 : `, mix[2]);
+// console.log(` mix 의 0번째 서랍 : `, mix[0]);
+// console.log(` mix 의 1번째 서랍 : `, mix[1]);
+// console.log(` mix 의 2번째 서랍 : `, mix[2]);
 
-console.log(`mix의 0번째 서랍의 이름:`, mix[0].name);
+// console.log(`mix의 0번째 서랍의 이름:`, mix[0].name);
 
-let jhard1 = {
-  name: "홍길동",
-  history: {
-    job: "야구선수",
-    year: "1999",
-  },
-};
+// let jhard1 = {
+//   name: "홍길동",
+//   history: {
+//     job: "야구선수",
+//     year: "1999",
+//   },
+// };
 
-jhard1.history.year = "2019"; //이게 위로 와야 밑에 1999결과값이 2019로 수정이 가능
+// console.log(`jhard : `, jhard1);
+// console.log(`jhard.name : `, jhard1.name);
+// console.log(`jhard.history:`, jhard1.history);
+// console.log(`jhard.history.year:`, jhard1.history.year);
 
-console.log(`jhard : `, jhard1);
-console.log(`jhard.name : `, jhard1.name);
-console.log(`jhard.history:`, jhard1.history);
-console.log(`jhard.history.year:`, jhard1.history.year);
+// jhard1.history.year = "2019";
+// // console.log(`jhard.history.year:`, jhard1.history.year);
+
+// // if (jhard1) {
+// //   console.log(`실행돼요`);
+// // }
+
+// // //@ts-ignore
+// // let password = "";
+// // if (password) {
+// //   console.log(`아아아아`);
+// // }
+
+// // if (!password) {
+// //   console.log(`어어어어`);
+// // }
---
+++ test5.ts
@@ -0,0 +1,35 @@
+// /* if 문 연습 */
+// // if (true & true) {
+// console.log(`&&연산결과에요`);
+// // }
+
+// let a = 1;
+// let b = 2;
+
+// if (a > b) {
+//   console.log(`a는 b보다 크다`);
+// }
+
+// if (a <= b) {
+//   console.log(`a는 b보다 같거나 작다`);
+// }
+
+// a = 1;
+// b = 2;
+
+// if (a >= b && b >= a) {
+//   console.log(`a>=b && b>=a 통과 될까요?`);
+// }
+// //통과 안됨
+
+// let score = 90;
+// if (score >= 90 && score <= 100) console.log(`A학점 입니다`);
+// else if (score <= 89 && score >= 80) {
+//   console.log(`B학점 입니다`);
+// } else if (score <= 79 && score >= 70) {
+//   console.log(`C학점 입니다`);
+// } else if (score <= 69 && score >= 60) {
+//   console.log(`D학점 입니다`);
+// } else {
+//   console.log(`F학점 입니다`);
+// }
