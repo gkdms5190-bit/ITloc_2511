42be48cdaa9d7eb9d2e73771159759a3fb263d24
gkdms5190-bit <gkdms5190@gmail.com>
2025-11-14 11:26:37 +0900

    .

--- test5_loop.ts
+++ test5_loop.ts
@@ -3,3 +3,11 @@
 for (let i = 1; i < 10; i++) {
   console.log(`i:`, i);
 }
+
+//구구단을 만들어보자요 9단까지//
+
+for (let i = 1; i <= 9; i++) {
+  for (let k = 1; k <= 9; k++) {
+    console.log(i * k);
+  }
+}
