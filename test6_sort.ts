578fcdbd8978234aa10f89f921ace4f37622a85f
gkdms5190-bit <gkdms5190@gmail.com>
2025-11-17 11:26:39 +0900

    .

---
+++ test6_sort.ts
@@ -0,0 +1,32 @@
+// /* 정렬 */
+// let nums = [1, 3, 5, 5, 3, 2, 45, 67, 6, 4543, 4, 34, 55, 65, 53, 34, 23];
+
+// for (let i = 0; i < nums.length; i++) {
+//   if (nums[i] > nums[i + 1]) {
+//     let temp = nums[i];
+//     nums[i] = nums[i + 1];
+//     nums[i + 1] = temp;
+//   }
+// }
+// console.log(`nums: `, nums);
+
+// for (let i = 0; i < nums.length; i++) {
+//   if (nums[i] > nums[i + 1]) {
+//     let temp = nums[i];
+//     nums[i] = nums[i + 1];
+//     nums[i + 1] = temp;
+//   }
+// }
+// console.log(`nums: `, nums);
+
+// for (let i = 0; i < nums.length; i++) {
+//   for (let k = 0; k < nums.length; k++) {
+//     if (nums[k] > nums[k + 1]) {
+//       let temp = nums[k];
+//       nums[k] = nums[k + 1];
+//       nums[k + 1] = temp;
+//     }
+//   }
+// }
+
+// console.log(`nums: `, nums);
